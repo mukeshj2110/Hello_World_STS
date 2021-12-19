8c496db3bbc0a27826fac2f7660693bb5ad81892
mukeshj2110 <mukeshjadhav680@gmail.com>
2021-12-19 16:41:15 +0530

    initial commit

---
+++ src/main/java/com/example/Hello_sts/HelloStsApplication.java
@@ -0,0 +1,13 @@
+package com.example.Hello_sts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloStsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloStsApplication.class, args);
+	}
+
+}
